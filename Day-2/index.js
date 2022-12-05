2bb26b2619198f0097960a5890779d278736f018
nosvalds <niko@devinit.org>
2022-12-05 11:19:23 +0000

    Day 2, Part 2

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,13 +1,24 @@
 import { assert } from 'console'
 import fs from 'fs'
 
+const map = {
+    A: 'X', B: 'Y', C: 'Z'
+}
+
+const loseMap = {
+    A: 'Z', B: 'X', C: 'Y'
+}
+
+const winMap = {
+    A: 'Y', B: 'Z', C: 'X'
+}
 const scoreMap = { 
                 A: 1, // Rock 
                 B: 2, // Paper
                 C: 3, // Scissors
-                X: 1, // Rock 
-                Y: 2, // Paper
-                Z: 3  // Scissors
+                X: 1, // Lose
+                Y: 2, // Draw
+                Z: 3  // Win
                 }
 
 const scores = [ 3, 0 , 6]
@@ -20,6 +31,18 @@ const scoreMatch = (player1, player2) => {
     return scores[outcome + 3] + scoreMap[player2]
 }
 
+const getPlayer2 = (player1, outcome) => {
+    if (outcome === 'Y') {
+        return map[player1]
+    }
+    if (outcome === 'X') {
+        return loseMap[player1]
+    }
+    if (outcome === 'Z') {
+        return winMap[player1]
+    }
+}
+
 const calulateScore = (filePath) => {
 
     const file = fs.readFileSync(filePath)
@@ -27,13 +50,14 @@ const calulateScore = (filePath) => {
     let fileArray = file.toString().split('\n')
     const matches = fileArray.map((val) => val.split(' '))
     return matches.reduce((totalScore, match) => {
-        return totalScore + scoreMatch(match[0], match[1])
+        const player2 = getPlayer2(match[0], match[1])
+        return totalScore + scoreMatch(match[0], player2)
     },0)
 }
 
 // Test
 console.log(calulateScore('example_input.txt'))
-assert(calulateScore('example_input.txt') === 15)
+assert(calulateScore('example_input.txt') === 12)
 
 // Answer 
 console.log(`Answer: ${calulateScore('input.txt')}`)
