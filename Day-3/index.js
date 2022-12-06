fff6962b21bb69fe55cfd407e09c62bb7451411b
nosvalds <niko@devinit.org>
2022-12-06 10:19:09 +0000

    Day 3, Part 2

--- Day-3/index.js
+++ Day-3/index.js
@@ -4,10 +4,12 @@ import fs from 'fs'
 const priority = 'abcdefghijklmnopqrstuvwxyz'
 const allPriority = priority + priority.toUpperCase()
 
-const findSimilarChar = (string1, string2) => {
+const getPriority = (char) => allPriority.indexOf(char) + 1
+
+const findSimilarChar = (string1, string2, string3) => {
     let res = ''
     string1.split('').every((char) => {
-        if (string2.includes(char)) { 
+        if (string2.includes(char) && string3.includes(char)) { 
             res = char
             return false
         }
@@ -23,14 +25,20 @@ const calulateTotalPriority = (filePath) => {
     const file = fs.readFileSync(filePath)
 
     let fileArray = file.toString().split('\n')
-    const matches = fileArray.reduce((total, val) => total + allPriority.indexOf(findSimilarChar(val.slice(0, val.length/2), val.slice(val.length/2))) + 1
-    , 0)
-    return matches
+    let count = 0
+    for (let index = 0; index < fileArray.length; index += 3) {
+        const elf1 = fileArray[index];
+        const elf2 = fileArray[index + 1];
+        const elf3 = fileArray[index + 2];
+        const char = findSimilarChar(elf1, elf2, elf3)
+        count += getPriority(char)
+    }
+    return count
 }
 
 // Test
 console.log(calulateTotalPriority('/Users/nosvalds/Projects/advent-of-code/Day-3/example_input.txt'))
-assert(calulateTotalPriority('example_input.txt') === 157)
+assert(calulateTotalPriority('example_input.txt') === 70)
 
 // Answer 
 console.log(`Answer: ${calulateTotalPriority('input.txt')}`)
